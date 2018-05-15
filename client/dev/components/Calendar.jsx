c055676382d3be8a78656cd877c85c7be16c80cd
Rory Eagan <rory.eagan@gmail.com>
2018-05-15 15:05:29 -0400

    Sort alphabetically by last name or by color.

--- client/dev/components/Calendar.jsx
+++ client/dev/components/Calendar.jsx
@@ -4,97 +4,155 @@ import PropTypes from 'prop-types';
 import CalenderRow from './CalendarRow';
 import styles from '../styles/styles.css';
 
-const Calendar = ({
-  schedule, employees, dayNum, weekNum, admin, copyDay, pasteDay, date, times, changeET, removeShift, addES,
-}) => {
-  Calendar.propTypes = {
-    schedule: PropTypes.array.isRequired,
-    employees: PropTypes.object.isRequired,
-    dayNum: PropTypes.number.isRequired,
-    weekNum: PropTypes.number.isRequired,
-    admin: PropTypes.bool.isRequired,
-    copyDay: PropTypes.func.isRequired,
-    pasteDay: PropTypes.func.isRequired,
-    date: PropTypes.instanceOf(Date).isRequired,
-    times: PropTypes.object.isRequired,
-    changeET: PropTypes.func.isRequired,
-    removeShift: PropTypes.func.isRequired,
-    addES: PropTypes.func.isRequired,
-  };
+export default class Calendar extends React.Component {
+  constructor(props) {
+    super(props);
 
-  const empOpts = [];
-  Object.keys(employees).sort((a, b) => {
-    const nameA = employees[a].name.toLowerCase();
-    const nameB = employees[b].name.toLowerCase();
-    if (nameA > nameB) {
-      return 1;
-    } else if (nameA < nameB) {
-      return -1;
-    }
-    return 0;
-  }).forEach((e) => {
-    if (!schedule[weekNum][dayNum].includes(Number(e))) {
-      empOpts.push(<option data-id={e}>{`${employees[e].name}`}</option>);
-    }
-  });
-  let thisDate = new Date(date.getTime());
-  thisDate.setDate(thisDate.getDate() + dayNum + (weekNum*7));
+    this.state = {
+      sort: 'Alphabetically',
+    };
 
-  const topRow = [...Array(27)].map((x, i) => {
-    let text = '';
-    if ((i - 1) % 2 === 0) {
-      text = `${8 + ((i - 1) / 2)}:00`;
+    this.changeSort = this.changeSort.bind(this);
+  }
+  
+  changeSort() {
+    this.setState({
+      sort: this.state.sort === 'Alphabetically' ? 'Color' : 'Alphabetically',
+    });
+  }
+
+  render() {
+    let {
+      schedule, employees, dayNum, weekNum, admin, copyDay, pasteDay, date, times, changeET, removeShift, addES,
+    } = this.props;
+    const empOpts = [];
+    Object.keys(employees).sort((a, b) => {
+      const nameA = employees[a].name.toLowerCase().split(' ')[1];
+      const nameB = employees[b].name.toLowerCase().split(' ')[1];
+      if (nameA > nameB) {
+        return 1;
+      } else if (nameA < nameB) {
+        return -1;
+      }
+      return 0;
+    }).forEach((e) => {
+      if (!schedule[weekNum][dayNum].includes(Number(e))) {
+        empOpts.push(<option data-id={e}>{`${employees[e].name}`}</option>);
+      }
+    });
+    let thisDate = new Date(date.getTime());
+    thisDate.setDate(thisDate.getDate() + dayNum + (weekNum*7));
+
+    const topRow = [...Array(27)].map((x, i) => {
+      let text = '';
+      if ((i - 1) % 2 === 0) {
+        text = `${8 + ((i - 1) / 2)}:00`;
+      } else {
+        text = `${7.5 + ((i - 1) / 2)}:30`;
+      }
+      if (i === 0) {
+        text = '';
+      }
+      return (
+        <div className={i === 0 ? styles.firstCol : styles.topRow}>
+          <span>
+            {i === 0 ?
+              <button
+                onClick={this.changeSort}
+              >
+                {this.state.sort === 'Alphabetically' ? 'Sort Color' : 'Sort Alpha'}
+              </button>
+              : text
+            }
+          </span>
+        </div>
+      );
+    });
+
+    let employeeRows = schedule[weekNum][dayNum].slice();
+    if (this.state.sort === 'Alphabetically') {
+      employeeRows.sort((a, b) => {
+        const nameA = employees[a].name.toLowerCase().split(' ')[1];
+        const nameB = employees[b].name.toLowerCase().split(' ')[1];
+        if (nameA > nameB) {
+          return 1;
+        } else if (nameA < nameB) {
+          return -1;
+        }
+        return 0;
+      });
     } else {
-      text = `${7.5 + ((i - 1) / 2)}:30`;
+      employeeRows.sort((a, b) => {
+        const nameA = employees[a].name.toLowerCase().split(' ')[1];
+        const nameB = employees[b].name.toLowerCase().split(' ')[1];
+        const colorA = employees[a].color;
+        const colorB = employees[b].color;
+
+        if (colorA > colorB) {
+          return 1;
+        } else if (colorB > colorA) {
+          return -1;
+        } else if (nameA > nameB) {
+          return 1;
+        } else if (nameA < nameB) {
+          return -1;
+        }
+        return 0;
+      });
     }
-    if (i === 0) {
-      text = '';
-    }
+
     return (
-      <div className={i === 0 ? styles.firstCol : styles.topRow}>
-        <span>
-          {text}
-        </span>
+      <div className={styles.calendar}>
+        <h3>{thisDate.toDateString()}</h3>
+        {topRow}
+        {employeeRows.map(e => (
+          <CalenderRow
+            key={e}
+            id={e}
+            data={employees[e]}
+            times={times}
+            changeTime={changeET}
+            dayNum={dayNum}
+            weekNum={weekNum}
+            removeShift={removeShift}
+            admin={admin}
+          />
+        ))}
+        {admin ?
+          <div>
+            Add Employee: <br />
+            <select onChange={(e) => {
+              if (e.target.selectedIndex !== 0) {
+                addES(e.target.options[e.target.selectedIndex].getAttribute('data-id'), e.target.value, dayNum, weekNum);
+                e.target.selectedIndex = 0;
+              }
+            }}
+            >
+              <option />
+              {empOpts}
+            </select>
+            <button onClick={() => copyDay(weekNum, dayNum)}>Copy</button>
+            <button onClick={() => pasteDay(weekNum, dayNum)}>Paste</button>
+          </div> :
+        ''}
       </div>
     );
-  });
+  }
+}
 
-  return (
-    <div className={styles.calendar}>
-      <h3>{thisDate.toDateString()}</h3>
-      {topRow}
-      {schedule[weekNum][dayNum].map(e => (
-        <CalenderRow
-          key={e}
-          id={e}
-          data={employees[e]}
-          times={times}
-          changeTime={changeET}
-          dayNum={dayNum}
-          weekNum={weekNum}
-          removeShift={removeShift}
-          admin={admin}
-        />
-      ))}
-      {admin ?
-        <div>
-          Add Employee: <br />
-          <select onChange={(e) => {
-            if (e.target.selectedIndex !== 0) {
-              addES(e.target.options[e.target.selectedIndex].getAttribute('data-id'), e.target.value, dayNum, weekNum);
-              e.target.selectedIndex = 0;
-            }
-          }}
-          >
-            <option />
-            {empOpts}
-          </select>
-          <button onClick={() => copyDay(weekNum, dayNum)}>Copy</button>
-          <button onClick={() => pasteDay(weekNum, dayNum)}>Paste</button>
-        </div> :
-      ''}
-    </div>
-  );
+Calendar.propTypes = {
+  schedule: PropTypes.array.isRequired,
+  employees: PropTypes.object.isRequired,
+  dayNum: PropTypes.number.isRequired,
+  weekNum: PropTypes.number.isRequired,
+  admin: PropTypes.bool.isRequired,
+  copyDay: PropTypes.func.isRequired,
+  pasteDay: PropTypes.func.isRequired,
+  date: PropTypes.instanceOf(Date).isRequired,
+  times: PropTypes.object.isRequired,
+  changeET: PropTypes.func.isRequired,
+  removeShift: PropTypes.func.isRequired,
+  addES: PropTypes.func.isRequired,
 };
 
-export default Calendar;
+// export default Calendar;
